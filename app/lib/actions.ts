8e73e764169f36db06c5379944e4587ee0d5b37f
DK <kuznetsenvy@gmail.com>
2025-05-03 21:29:14 -0300

    Chapter 14. Improving Accessibility - added validation to `edit-form.tsx` component.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -81,13 +81,22 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -99,10 +108,11 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
     `;
   } catch (error) {
-    // We'll log the error to the console for now
-    console.error(error);
+    // If a database error occurs, return a more specific error.
+    return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
