18d5efc278bfd82ca55824e9db88c40daa3b0d31
DK <kuznetsenvy@gmail.com>
2025-05-03 22:55:24 -0300

    Fixed lint errors for clean Vercel deployments

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -72,6 +72,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
+    console.log(error);
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -111,6 +112,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
         WHERE id = ${id}
     `;
   } catch (error) {
+    console.log(error);
     // If a database error occurs, return a more specific error.
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
