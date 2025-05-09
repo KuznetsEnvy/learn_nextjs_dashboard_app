0bccd5db0f2d3ebafe390f4c886c3abf2350058c
DK <kuznetsenvy@gmail.com>
2025-05-09 01:23:15 -0300

    Changed `invoices.date` SQL column to timestamp to keep order of invoices. Had to temporarily remove `middleware.ts` to prevent redirect while visiting `app/seed/update/route.ts`

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -62,13 +62,12 @@ export async function createInvoice(prevState: State, formData: FormData) {
   // Prepare data for insertion into the database
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
 
   // Insert data into the database
   try {
     await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        INSERT INTO invoices (customer_id, amount, status)
+        VALUES (${customerId}, ${amountInCents}, ${status})
     `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
